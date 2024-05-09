4ac82a3d118a8e42025de4010f6c13211248bf51
Andrew <linkegamer@gmail.com>
2024-05-09 09:05:37 +0300

    singletonclient

---
+++ singletonclient/main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include "singletonclient.h"
+#include <iostream>
+#include <QString>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    std::string msg;
+    std::cin >> msg;
+    SingletonClient::getInstance()->
+        send_msg_to_server(QString::fromStdString(msg));
+
+    return a.exec();
+}
